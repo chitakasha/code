6b224248d50387cf8fe2df4af24f6a51af178e34
Pasha Mango <141678664+pashamango@users.noreply.github.com>
2023-08-29 14:57:22 -0700

    Update recombinantai_validation.py

--- Utils/C-Utils/recombinantai_validation.py
+++ Utils/C-Utils/recombinantai_validation.py
@@ -3,14 +3,19 @@
 # description: This script validates the generated README.md files using RecombinantAI.
 
 import os
+import difflib
 
 def validate_readme_with_recombinantai():
     """
     This function validates the generated README.md files.
     """
     
+    # Read the README_template.md file
+    with open("/workspaces/code/Utils/C-Utils/README_template.md", "r") as template_file:
+        template_content = template_file.read()
+    
     # Define the root directory from where to start validating README files
-    root_dir = "/path/to/ChitAkasha/project"  # Specify the actual path
+    root_dir = "/workspaces/code"  # Specify the actual path based on your project structure
     
     # Loop through each directory and sub-directory to validate README.md files
     for dirpath, dirnames, filenames in os.walk(root_dir):
@@ -28,8 +33,12 @@ def validate_readme_with_recombinantai():
             content = readme_file.read()
             
             # Perform validation (this is where you can integrate RecombinantAI)
-            if "Role and Structure" not in content:
-                print(f"Validation failed for {dirpath}: 'Role and Structure' section missing.")
+            # For now, we'll just compare the content with the template
+            diff = difflib.ndiff(template_content.splitlines(), content.splitlines())
+            differences = [l for l in diff if l.startswith('+ ') or l.startswith('- ')]
+            
+            if differences:
+                print(f"Validation failed for {dirpath}: README does not match the template.")
             else:
                 print(f"Validation succeeded for {dirpath}")
 
