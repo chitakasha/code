8a5b255887fbdfae4243489ba9c6da416e18fe43
Pasha Mango <141678664+pashamango@users.noreply.github.com>
2023-08-29 16:57:28 +0000

    Commit all changes

---
+++ create_chitakasha_structure.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Read instructions.txt line by line
+while read -r line; do
+  # Skip comments
+  [[ "$line" =~ ^#.*$ ]] && continue
+
+  # Create directory or file based on the instruction
+  if [[ "$line" =~ ^DIR ]]; then
+    mkdir -p "${line#DIR }"
+  elif [[ "$line" =~ ^FILE ]]; then
+    touch "${line#FILE }"
+  fi
+done < instructions.txt
